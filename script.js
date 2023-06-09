c7fd4cf4fcf1725deae5c28e7bd536c1035e43ac
Thiago Gabriel <88698375+thiagogab@users.noreply.github.com>
2023-06-08 21:07:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function countText() {
+    let text = document.form_main.text.value;
+    document.getElementById('characters').innerText = text.length;
+    document.getElementById('words').innerText = text.length == 0 ? 0 : text.split(/\s+/).length;
+    document.getElementById('rows').innerText = text.length == 0 ? 0 : text.split(/\n/).length;
+  }
